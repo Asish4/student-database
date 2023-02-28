8ba3bc563692fe1d9f204dcfb1c55bd1703e919f
Asish Patra <asishpatra34@gmail.com>
2023-02-28 11:59:50 +0530

    Add colors to the toast and change position

--- JavaScript/studentdata.js
+++ JavaScript/studentdata.js
@@ -3,104 +3,98 @@ var x;
 var y;
 var c;
 
+function displayDetails() {
+
+    var id = document.getElementById("employee_id");
+    if (validateID(id)) {
+        var idv = id.value;
+    }
+    var name = document.getElementById("name");
+    if (validateName(name)) {
+        var namev = name.value;
+    }
+    var ph = document.getElementById("mobile_number");
+    if (validatePh(ph)) {
+        var phv = ph.value;
+    }
+    var mail = document.getElementById("email-id");
+    if (validateMail(mail)) {
+        var mailv = mail.value;
+    }
+    var city = document.getElementById("city-id");
+    if (validateCity(city)) {
+        var cityv = city.value;
+    }
+    var state = document.getElementById("state");
+    if (validateState(state)) {
+        var statev = state.value;
+    }
+    var country = document.getElementById("country");
+    if (validateCountry(country)) {
+        var countryv = country.value;
+    }
+    var pin = document.getElementById("pin_code");
+    if (validatePIN(pin)) {
+        var pinv = pin.value;
+    }
 
 
+    if (validateAll(id, name, ph, mail, city, state, country, pin)) {
+        var employeId = document.getElementById("employee_id").value;
+        var name = document.getElementById("name").value;
+        var mobileNumber = document.getElementById("mobile_number").value;
+        var email = document.getElementById("email").value;
+        var city = document.getElementById("city").value;
+        var state = document.getElementById("state").value;
+        var country = document.getElementById("country").value;
+        var pinCode = document.getElementById("pin_code").value;
+        // var pinCode = document.getElementById("pin_code").value;
+
+        // Storing the table elements in a variable using table is
+        var takeId = document.getElementById("table_id");
+
+        // Creating a tr(Table row) element and store the element in row variable
+        var row = document.createElement("tr");
+        // Adding a row(child) to the table(patrnt)
+        takeId.appendChild(row);
+
+        // add the "td"(Child) to the "tr"(Parent) and stote the "td" elements in a vaiable like col0
+        var col0 = row.appendChild(document.createElement("td"));
+        var col1 = row.appendChild(document.createElement("td"));
+        var col2 = row.appendChild(document.createElement("td"));
+        var col3 = row.appendChild(document.createElement("td"));
+        var col4 = row.appendChild(document.createElement("td"));
+        var col5 = row.appendChild(document.createElement("td"));
+        var col6 = row.appendChild(document.createElement("td"));
+        var col7 = row.appendChild(document.createElement("td"));
+        var col8 = row.appendChild(document.createElement("td"));
+        var col9 = row.appendChild(document.createElement("td"));
+
+        // Add data in column
+        col0.innerHTML = employeId;
+        col1.innerHTML = name;
+        col2.innerHTML = mobileNumber;
+        col3.innerHTML = email;
+        col4.innerHTML = city;
+        col5.innerHTML = state;
+        col6.innerHTML = country;
+        col7.innerHTML = pinCode;
+        col8.innerHTML =
+            '<button type="button" class="btn btn-outline-info" data-toggle="modal" onclick="myFunction(this); showValues();"  data-target="#editModal"><i class="fa-solid fa-pen-to-square"></i></button>';
+        col9.innerHTML =
+            '<button type="button" class="btn btn-outline-danger" data-toggle="modal" onclick="myFunction(this)" data-target="#exampleModal"> <i class="fa-solid fa-trash"></i></button>';
+    }
+}
+
+//Show Submit toast
+function showSubmitToast() {
+    console.log("Working");
+    var a = document.getElementById("submit_toast");
+    var b = new bootstrap.Toast(a);
+    b.show();
+}
 
 
-   
-       
-        function displayDetails() {
-
-            var id = document.getElementById("employee_id");
-            if (validateID(id)) {
-                var idv = id.value;
-            }
-            var name = document.getElementById("name");
-            if (validateName(name)) {
-                var namev = name.value;
-            }
-            var ph = document.getElementById("mobile_number");
-            if (validatePh(ph)) {
-                var phv = ph.value;
-            }
-            var mail = document.getElementById("email-id");
-            if (validateMail(mail)) {
-                var mailv = mail.value;
-            }
-            var city = document.getElementById("city-id");
-            if (validateCity(city)) {
-                var cityv = city.value;
-            }
-            var state = document.getElementById("state");
-            if (validateState(state)) {
-                var statev = state.value;
-            }
-            var country = document.getElementById("country");
-            if (validateCountry(country)) {
-                var countryv = country.value;
-            }
-            var pin = document.getElementById("pin_code");
-            if (validatePIN(pin)) {
-                var pinv = pin.value;
-            }
-            
-                
-            if (validateAll(id, name, ph, mail, city, state, country, pin)) {
-            var employeId = document.getElementById("employee_id").value;
-            var name = document.getElementById("name").value;
-            var mobileNumber = document.getElementById("mobile_number").value;
-            var email = document.getElementById("email").value;
-            var city = document.getElementById("city").value;
-            var state = document.getElementById("state").value;
-            var country = document.getElementById("country").value;
-            var pinCode = document.getElementById("pin_code").value;
-            // var pinCode = document.getElementById("pin_code").value;
-
-            // Storing the table elements in a variable using table is
-            var takeId = document.getElementById("table_id");
-
-            // Creating a tr(Table row) element and store the element in row variable
-            var row = document.createElement("tr");
-            // Adding a row(child) to the table(patrnt)
-            takeId.appendChild(row);
-
-            // add the "td"(Child) to the "tr"(Parent) and stote the "td" elements in a vaiable like col0
-            var col0 = row.appendChild(document.createElement("td"));
-            var col1 = row.appendChild(document.createElement("td"));
-            var col2 = row.appendChild(document.createElement("td"));
-            var col3 = row.appendChild(document.createElement("td"));
-            var col4 = row.appendChild(document.createElement("td"));
-            var col5 = row.appendChild(document.createElement("td"));
-            var col6 = row.appendChild(document.createElement("td"));
-            var col7 = row.appendChild(document.createElement("td"));
-            var col8 = row.appendChild(document.createElement("td"));
-            var col9 = row.appendChild(document.createElement("td"));
-
-            // Add data in column
-            col0.innerHTML = employeId;
-            col1.innerHTML = name;
-            col2.innerHTML = mobileNumber;
-            col3.innerHTML = email;
-            col4.innerHTML = city;
-            col5.innerHTML = state;
-            col6.innerHTML = country;
-            col7.innerHTML = pinCode;
-            col8.innerHTML =
-                '<button type="button" class="btn btn-outline-info" data-toggle="modal" onclick="myFunction(this); showValues();"  data-target="#editModal"><i class="fa-solid fa-pen-to-square"></i></button>';
-            col9.innerHTML =
-                '<button type="button" class="btn btn-outline-danger" data-toggle="modal" onclick="myFunction(this)" data-target="#exampleModal"> <i class="fa-solid fa-trash"></i></button>';
-        }
-        }
-
-        //Show Submit toast
-        function showSubmitToast() {
-            console.log("Working");
-            var a = document.getElementById("submit_toast");
-            var b = new bootstrap.Toast(a);
-            b.show();
-        }
- 
-    
 
 
 function myFunction(i) {
@@ -160,27 +154,27 @@ function showDetails() {
     }
 
     if (validatemAll(name, ph, mail, city, state, country, pin)) {
-    var employeId = document.getElementById("employee_ids").value;
-    var name = document.getElementById("name_ids").value;
-    var mobileNumber = document.getElementById("mobile_ids").value;
-    var email = document.getElementById("email_ids").value;
-    var city = document.getElementById("city_ids").value;
-    var state = document.getElementById("state_ids").value;
-    var country = document.getElementById("country_ids").value;
-    var pinCode = document.getElementById("p_id").value;
+        var employeId = document.getElementById("employee_ids").value;
+        var name = document.getElementById("name_ids").value;
+        var mobileNumber = document.getElementById("mobile_ids").value;
+        var email = document.getElementById("email_ids").value;
+        var city = document.getElementById("city_ids").value;
+        var state = document.getElementById("state_ids").value;
+        var country = document.getElementById("country_ids").value;
+        var pinCode = document.getElementById("p_id").value;
 
-    a.childNodes[0].innerHTML = employeId;
-    a.childNodes[1].innerHTML = name;
-    a.childNodes[2].innerHTML = mobileNumber;
-    a.childNodes[3].innerHTML = email;
-    a.childNodes[4].innerHTML = city;
-    a.childNodes[5].innerHTML = state;
-    a.childNodes[6].innerHTML = country;
-    a.childNodes[7].innerHTML = pinCode;
+        a.childNodes[0].innerHTML = employeId;
+        a.childNodes[1].innerHTML = name;
+        a.childNodes[2].innerHTML = mobileNumber;
+        a.childNodes[3].innerHTML = email;
+        a.childNodes[4].innerHTML = city;
+        a.childNodes[5].innerHTML = state;
+        a.childNodes[6].innerHTML = country;
+        a.childNodes[7].innerHTML = pinCode;
 
-    col8.innerHTML =
-        '<button type="button" class="btn btn-outline-danger" data-toggle="modal" onclick="myFunction(this)" data-target="#exampleModal" ><i class="fa-solid fa-trash"></i></button><button type="button" class="btn btn-outline-info" data-toggle="modal" onclick="myFunction(this) showValues();"  data-target="#editModal" ><i class="fa-solid fa-pen-to-square"></i></button>';
-}
+        col8.innerHTML =
+            '<button type="button" class="btn btn-outline-danger" data-toggle="modal" onclick="myFunction(this)" data-target="#exampleModal" ><i class="fa-solid fa-trash"></i></button><button type="button" class="btn btn-outline-info" data-toggle="modal" onclick="myFunction(this) showValues();"  data-target="#editModal" ><i class="fa-solid fa-pen-to-square"></i></button>';
+    }
 }
 
 //Show values on modal form
